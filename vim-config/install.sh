12c7aec1e82bd0b1eb023d4145ed1f4e9fe4bce5
felix <agyemang.17@wright.edu>
2026-04-23 00:57:15 -0400

    added commits install.sh

--- vim-config/install.sh
+++ vim-config/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
+# Copy vimrc from the repo to the home directory
 echo "Deploying .vimrc..."
 cp vimrc ~/.vimrc
 
-
+# Check if Vundle (Vim plugin manager) is already installed
 VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
-if [ ! -d "$VUNDLE_DIR" ]; then
+if [ ! -d "$VUNDLE_DIR" ]; then  # Clone Vundle from GitHub if it doesn't exist
     echo "Cloning Vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
 fi
 
-
+# Launch Vim to automatically install all listed plugins, then quit
 echo "Installing plugins..."
 vim +PluginInstall +qall
 
